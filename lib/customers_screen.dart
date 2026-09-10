8ffd63d407703b71d32f0af051aac316bdb5d974
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 16:54:39 +0300

    Update customers_screen.dart

--- lib/customers_screen.dart
+++ lib/customers_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class customersscreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const customersscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
