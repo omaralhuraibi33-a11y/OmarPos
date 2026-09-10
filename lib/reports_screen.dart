2fd4062905486247e965f567d025d1615ca2da93
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:18:55 +0300

    Update reports_screen.dart

--- lib/reports_screen.dart
+++ lib/reports_screen.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 
-class reportsscreen extends StatelessWidget {
-  const reportsscreen({super.key});
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(' التقارير'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('التقارير'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة التقارير', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
-
