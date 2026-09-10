520bf34d55362b01ebccaf9a64d098dc49cefb4d
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:19:45 +0300

    Update suppliers_screen.dart

--- lib/suppliers_screen.dart
+++ lib/suppliers_screen.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 
-class suppliersscreen extends StatelessWidget {
-  const suppliersscreen({super.key});
+class SuppliersScreen extends StatelessWidget {
+  const SuppliersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('الموردين'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('الموردين'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة الموردين', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
