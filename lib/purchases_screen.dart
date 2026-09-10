9dd5605dc615b25b557673128900c4e1d815ffae
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 15:05:03 +0300

    Create purchases_screen.dart

---
+++ lib/purchases_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class purchasesscreen extends StatelessWidget {
+  const PosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('المشتريات'),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Text(
+          'شاشة قيد التطوير',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
