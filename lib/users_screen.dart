7b9681dcd27568d7f82294b684d0e2348ea398f1
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:02:32 +0300

    Update users_screen.dart

--- lib/users_screen.dart
+++ lib/users_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class usersscreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const usersscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
