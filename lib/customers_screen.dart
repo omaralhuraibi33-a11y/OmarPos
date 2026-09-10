22c28e3e975c7486cec25a4503616494245be54a
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:20:47 +0300

    Update customers_screen.dart

--- lib/customers_screen.dart
+++ lib/customers_screen.dart
@@ -1,22 +1,14 @@
+import 'package0/flutter/material.dart'; // تم تصحيحها لـ package:flutter
 import 'package:flutter/material.dart';
 
-class customersscreen extends StatelessWidget {
-  const customersscreen({super.key});
+class CustomersScreen extends StatelessWidget {
+  const CustomersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('العملاء'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('العملاء'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة العملاء', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
