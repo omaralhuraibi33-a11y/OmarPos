5b21ca501fbcc3d38437464a9f53e776bf5b745b
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:12:32 +0300

    Update cash_box_screen.dart

--- lib/cash_box_screen.dart
+++ lib/cash_box_screen.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 
-class cashboxscreen extends StatelessWidget {
-  const cashboxscreen({super.key});
+class CashBoxScreen extends StatelessWidget {
+  const CashBoxScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(' الصندوق'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('الصندوق'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة الصندوق', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
