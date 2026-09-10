e700cfe078cbc4598699a3b8acee639a102d8152
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 16:58:24 +0300

    Update reports_screen.dart

--- lib/reports_screen.dart
+++ lib/reports_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class reportsscreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const reportsscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
