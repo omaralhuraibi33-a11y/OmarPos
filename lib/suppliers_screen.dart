5a5ac253fb40d91a86194b9d3f05896a4684a44d
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:01:22 +0300

    Update suppliers_screen.dart

--- lib/suppliers_screen.dart
+++ lib/suppliers_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class suppliersscreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const suppliersscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
