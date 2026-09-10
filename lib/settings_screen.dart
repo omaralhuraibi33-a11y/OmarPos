0bb81dfd65ccd1cfd664cb11053ef47604ca1f2f
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:18:08 +0300

    Update settings_screen.dart

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 
-class settingsscreen extends StatelessWidget {
-  const settingsscreen({super.key});
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(' إعدادات النظام'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('إعدادات النظام'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة إعدادات النظام', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
