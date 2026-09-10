1af61fe033a2984564cd6d3429aba1e49eca6c3c
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 16:59:36 +0300

    Update shift_close_screen.dart

--- lib/shift_close_screen.dart
+++ lib/shift_close_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class shiftclosescreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const shiftclosescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
