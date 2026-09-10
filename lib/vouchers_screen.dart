7dea451f86169cc8edf9cf3e91bcb32da61d5632
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:13:44 +0300

    Update vouchers_screen.dart

--- lib/vouchers_screen.dart
+++ lib/vouchers_screen.dart
@@ -1,22 +1,14 @@
+import 'package0/flutter/material.dart';
 import 'package:flutter/material.dart';
 
-class vouchersscreen extends StatelessWidget {
-  const vouchersscreen({super.key});
+class VouchersScreen extends StatelessWidget {
+  const VouchersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('السندات'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('السندات'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة السندات', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
