08e8492d0bef0f976d68e68bbe8b31c9d99130e2
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 16:59:01 +0300

    Update settings_screen.dart

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class settingsscreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const settingsscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
