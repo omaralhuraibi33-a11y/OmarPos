e66dd4d889316674458870024c7d4aa5acaded3f
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:14:41 +0300

    Update financial_report_screen.dart

--- lib/financial_report_screen.dart
+++ lib/financial_report_screen.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 
-class financialreportscreen extends StatelessWidget {
-  const financialreportscreen({super.key});
+class FinancialReportScreen extends StatelessWidget {
+  const FinancialReportScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(' التقرير المالي'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('التقرير المالي'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة التقرير المالي', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
