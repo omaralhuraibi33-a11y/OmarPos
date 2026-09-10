abd26e177a5d4d80f31ea8ac1992fb1d5864e8e8
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 16:56:17 +0300

    Update inventory_screen.dart

--- lib/inventory_screen.dart
+++ lib/inventory_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class inventoryscreen extends StatelessWidget {
-  const PosScreen({super.key});
+  const inventoryscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
