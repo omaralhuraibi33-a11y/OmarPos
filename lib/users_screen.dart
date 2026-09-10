26a3e4cad6dfd88fde144342fb0f83a3d1489dde
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 16:20:34 +0300

    Create users_screen.dart

---
+++ lib/users_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class usersscreen extends StatelessWidget {
+  const PosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(' إدارة المستخدمين'),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Text(
+          'شاشة قيد التطوير',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
