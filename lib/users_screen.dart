329c99a5de5090013fa7181e7caec2d5b6b5764f
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-10 17:17:10 +0300

    Update users_screen.dart

--- lib/users_screen.dart
+++ lib/users_screen.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
 
-class usersscreen extends StatelessWidget {
-  const usersscreen({super.key});
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(' إدارة المستخدمين'),
-        backgroundColor: Colors.indigo,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'شاشة قيد التطوير',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ),
+      appBar: AppBar(title: const Text('إدارة المستخدمين'), backgroundColor: Colors.indigo, foregroundColor: Colors.white),
+      body: const Center(child: Text('شاشة إدارة المستخدمين', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
     );
   }
 }
