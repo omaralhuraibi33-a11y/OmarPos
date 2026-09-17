7d88d61f9afcf515be938107b215f33e28195f9c
omaralhuraibi33-a11y <omaralhuraibi33@gmail.com>
2026-09-17 16:47:41 +0300

    Update pos_screen.dart

--- lib/pos_screen.dart
+++ lib/pos_screen.dart
@@ -22,7 +22,7 @@ class CartItem {
 }
 
 class PosScreen extends StatefulWidget {
-  const PosScreen({Key? key}) : super(Key: key);
+  const PosScreen({super.key});
   @override
   State<PosScreen> createState() => _PosScreenState();
 }
